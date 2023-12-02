fa4f59f57968c6cf8aff8b88d75ff3a540a7e485
Leonardo Freitas dos Santos <leonardo.freitas.s@outlook.com>
2023-12-02 11:59:46 +0100

    feat: day 2 part 2

--- src/adventofcode_2023/exercises/day_2/part_2.clj
+++ src/adventofcode_2023/exercises/day_2/part_2.clj
@@ -1,4 +1,30 @@
-(ns adventofcode-2023.exercises.day-2.part-2)
+(ns adventofcode-2023.exercises.day-2.part-2 
+  (:require [clojure.string :as str]))
+
+(defn set-cube-power
+  [rounds]
+  (reduce
+   *
+   (vals
+    (reduce
+     (fn [acc round]
+       (reduce
+        (fn [acc dice]
+          (let [[n color] (str/split dice #" ")]
+            (update acc color max (Integer/parseInt n))))
+        acc
+        (str/split round #", "))) 
+       {"green" 0
+        "red" 0
+        "blue" 0}
+     rounds))))
 
 (defn run
-  [inputs])
+  [inputs]
+  (reduce
+   (fn [acc line]
+     (let [[_ game] (re-find #"Game [\d]+: (.*)" line)
+           rounds (str/split game #"; ")]
+       (+ acc (set-cube-power rounds))))
+   0
+   inputs))
