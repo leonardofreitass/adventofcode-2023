eda80e8faf97b06bfd6f0e81ab70ba35cef5b368
Leonardo Freitas dos Santos <leonardo.freitas.s@outlook.com>
2023-12-06 13:47:29 +0100

    feat: day 6 part 2 (math refactor)

--- src/adventofcode_2023/exercises/day_6/part_2.clj
+++ src/adventofcode_2023/exercises/day_6/part_2.clj
@@ -1,16 +1,14 @@
 (ns adventofcode-2023.exercises.day-6.part-2 
   (:require [clojure.string :as str]))
 
-(defn travel
-  [t p]
-  (* p (- t p)))
-
-(defn first-best
-  [t d r]
-  (some #(if (> (travel t %) d) % false) r))
+(defn quadratic-function
+  [t d]
+  (Math/sqrt (- (Math/pow t 2) (* 4 d))))
 
 (defn run
   [inputs]
   (let [t (bigint (str/join "" (re-seq #"[\d]+" (first inputs))))
         d (bigint (str/join "" (re-seq #"[\d]+" (second inputs))))]
-    (inc (- (first-best t d (range t 0 -1)) (first-best t d (range t))))))
+    (dec (-
+          (Math/ceil (/ (+ t (quadratic-function t d)) 2))
+          (Math/floor (/ (- t (quadratic-function t d)) 2))))))
