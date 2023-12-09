0126dbc5485e76185472a561ec9fd585125ed4f4
Leonardo Freitas dos Santos <leonardo.freitas.s@outlook.com>
2023-12-09 11:01:18 +0100

    feat: day 9 part 2

---
+++ src/adventofcode_2023/exercises/day_9/part_2.clj
@@ -0,0 +1,21 @@
+(ns adventofcode-2023.exercises.day-9.part-2)
+
+(defn lazy-diff
+  [coll]
+  (map (fn [[a b]] (- a b)) (partition 2 1 coll)))
+
+(defn parse-inputs
+  [inputs]
+  (map
+   (fn [line]
+     (map #(Integer/parseInt %) (re-seq #"[\-\d]+" line)))
+   inputs))
+
+(defn find-next
+  [coll]
+  (let [diffs (lazy-diff coll)]
+    (if (every? zero? diffs) (first coll) (+ (first coll) (find-next diffs)))))
+
+(defn run
+  [inputs]
+  (reduce + (map find-next (parse-inputs inputs))))
